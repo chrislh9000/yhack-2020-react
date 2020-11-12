3a4224cd2a26218e513a2baec2750e1228841fbc
Kendrick Tan <kendricktan38@gmail.com>
2020-11-12 15:23:12 +0800

    audio

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Music from './Music';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Music />
   </React.StrictMode>,
   document.getElementById('root')
 );
