882dfcb9afeb5d346bae9b44315bf68fee47d9eb
Christopher Lee Hemm <chemm@Christophers-MacBook-Pro-3.local>
2020-12-17 23:38:46 +0800

    added podcast storage and download functionality

--- env.sh
+++ env.sh
@@ -1,1 +1,3 @@
 export MONGODB_URI="mongodb+srv://chrislh2000:!y24vw9zJ@cluster0.c1ute.mongodb.net/Pincast?retryWrites=true&w=majority"
+export CLOUDINARY_API_KEY="694445253517932"
+export CLOUDINARY_API_SECRET="E5veH5IB1VNYkxp3jxEeCM34LCA"
