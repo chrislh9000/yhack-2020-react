43ae60a99cc7977de6938de72840bf6438c6898c
Christopher Lee Hemm <chemm@Christophers-MacBook-Pro-3.local>
2020-12-21 17:11:20 +0800

    added new schemas

--- server/models.js
+++ server/models.js
@@ -11,6 +11,11 @@ const UserSchema = new Schema({
   password: {
     type: String,
     required: true,
+  },
+  pins: {
+    type: Schema.Types.ObjectId,
+    ref: 'Pin',
+    default: []
   }
 });
 
@@ -24,11 +29,73 @@ const GcloudResponseSchema = new Schema({
   }
 });
 
+const PinSchema = new Schema({
+  test : {
+    type: String,
+    required: true
+  },
+  startTime: {
+    type: Integer,
+    required: true
+  },
+  endTime: {
+    type: Integer,
+    required: true
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  podcast: {
+    type: Schema.Types.ObjectId,
+    ref: 'Podcast',
+    required: true
+  }
+})
+
+const PodcastSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  transcript: {
+    type: Schema.Types.ObjectId,
+    ref: 'GcloudResponse',
+    required: true
+  }
+})
+
+const PinShareSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  from: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  pin: {
+    type: Schema.Types.ObjectId,
+    ref: 'Pin',
+    required: true
+  }
+})
+
 const User = mongoose.model('User', UserSchema);
 const GcloudResponse = mongoose.model('GcloudResponse', GcloudResponseSchema);
+const Pin = mongoose.model('Pin', PinSchema);
+const Podcast = mongoose.model('Podcast', PodcastSchema);
+const Pinshare = mongoose.model('Pinshare', PinShareSchema);
+
 
 
 export default {
   User: User,
-  GcloudResponse: GcloudResponse
+  GcloudResponse: GcloudResponse,
+  Pin: Pin,
+  Podcast: Podcast,
+  Pinshare: Pinshare
 };
